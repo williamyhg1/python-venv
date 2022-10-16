e639ecf318706b90e469ad42228a8424372d4dc6
williamyhg1 <90809764+williamyhg1@users.noreply.github.com>
2022-10-16 17:12:02 +1100

    finish while loop

--- main.py
+++ main.py
@@ -1,19 +1,34 @@
+
+
 def days_to_units(days): 
     return int(days) * 24
 
 def validate_and_execute():
     try:
-        user_input_number = int(input("Please enter a number of days and I will convert it to hours!\n"))
-        if (user_input_number) > 0:
-            calculated_value = days_to_units(user_input_number)
-            print(f"{user_input_number} days equal to {calculated_value} hours")
-        elif user_input_number == 0:
-            print("You entered a 0, please enter a valid positive number")
-        else:
-            print("You entered a negative number.")
+        user_input_number = set(input("Please enter a number of days and I will convert it to hours!\n").split(","))
+        print(user_input_number)
+        if len(user_input_number) > 0:
+            for unit in user_input_number:
+                if int(unit) == 0:
+                    print("You entered a 0, please enter a valid positive number")
+                elif int(unit) < 0:
+                    print("You entered a negative number.")    
+                else: 
+                    calculated_value = days_to_units(unit)
+                    print(f"{unit} days equal to {calculated_value} hours")
+                
     except:
         print("Your input is not a valid number.")
         
+switch = True
 
 
-validate_and_execute()
+while switch:
+    validate_and_execute()
+    exit=str.lower(input("Do you want to exit?"))
+    if exit == "yes":
+        switch = False
+    elif exit == "no":
+        switch =True
+    else:
+        switch = True
